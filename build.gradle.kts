f89d3e7edfd899d0dbaefa892b9a8a3fca70bdbc
zjun <565061763@qq.com>
2024-05-10 01:20:52 +0800

    refactor

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,31 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
 plugins {
-    alias(libs.plugins.jetbrainsCompose) apply false
-    alias(libs.plugins.kotlinMultiplatform) apply false
+    kotlin("jvm")
+    id("org.jetbrains.compose")
+}
+
+repositories {
+    google()
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+}
+
+dependencies {
+    implementation(compose.desktop.currentOs)
+    implementation(compose.materialIconsExtended)
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-swing:1.6.0")
+    implementation("com.squareup.moshi:moshi-kotlin:1.15.1")
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "VasDollyHelper"
+            packageVersion = "1.0.0"
+        }
+    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,31 +1,11 @@
-rootProject.name = "VasDollyHelper"
-enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
-
 pluginManagement {
     repositories {
-        google {
-            mavenContent {
-                includeGroupAndSubgroups("androidx")
-                includeGroupAndSubgroups("com.android")
-                includeGroupAndSubgroups("com.google")
-            }
-        }
-        mavenCentral()
         gradlePluginPortal()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+
+    plugins {
+        kotlin("jvm").version(extra["kotlin.version"] as String)
+        id("org.jetbrains.compose").version(extra["compose.version"] as String)
     }
 }
-
-dependencyResolutionManagement {
-    repositories {
-        google {
-            mavenContent {
-                includeGroupAndSubgroups("androidx")
-                includeGroupAndSubgroups("com.android")
-                includeGroupAndSubgroups("com.google")
-            }
-        }
-        mavenCentral()
-    }
-}
-
-include(":app")
