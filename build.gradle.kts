ffa88edf41218ef24fccdded69bf53a80534f4cb
zjun <565061763@qq.com>
2024-05-10 09:08:14 +0800

    mainClass

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ dependencies {
 
 compose.desktop {
     application {
-        mainClass = "MainKt"
+        mainClass = "com.sd.app.vasdolly.MainKt"
 
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
